d31960eb11a83df1eb9843351d1fb7fb90a7c24f
Laszlo Teveli <laszlo.teveli@skyscanner.net>
2018-01-03 01:47:43 +0100

    Refactor

--- Sources/Interpreter/Utilities/Matcher.swift
+++ Sources/Interpreter/Utilities/Matcher.swift
@@ -18,37 +18,59 @@ public class Matcher<T, E: VariableEvaluator> {
         self.elements = elements
     }
     
-    public func matches(string: String, from start: Int = 0, until length: Int = 1, interpreter: E) -> MatchResult<T> {
+    public func matches(string: String, from start: Int = 0, until length: Int, interpreter: E) -> MatchResult<T> {
         let isLast = string.count == start + length
-        let prefix = String(string[start ..< start + length])
+        let trimmed = String(string[start ..< start + length])
         var elementIndex = 0
-        var input = prefix
+        var remainder = trimmed
         var variables: [String: Any] = [:]
-        var currentlyActiveVariable: (name: String, value: String, interpreted: Bool, acceptsNilValue: Bool, map: (Any, Any) -> Any?)? = nil
+        
+        typealias VariableValue = (metadata: VariableProtocol, value: String)
+        var currentlyActiveVariable: VariableValue? = nil
+        func tryToAppendCurrentVariable() -> Bool {
+            if let variable = currentlyActiveVariable {
+                appendNextCharacterToVariable(variable)
+            }
+            return currentlyActiveVariable != nil
+        }
+        func appendNextCharacterToVariable(_ variable: VariableValue) {
+            if !remainder.isEmpty {
+                currentlyActiveVariable = (variable.metadata, variable.value + String(remainder.removeFirst()))
+            }
+        }
+        func initialiseVariable(_ element: MatchElement) {
+            if currentlyActiveVariable == nil, let variable = element as? VariableProtocol {
+                appendNextCharacterToVariable((variable, ""))
+            }
+        }
+        func registerAndValidateVariable() -> Bool {
+            if let variable = currentlyActiveVariable {
+                variables[variable.metadata.name] = finaliseVariable(variable, interpreter: interpreter)
+                return !variable.metadata.acceptsNilValue && variables[variable.metadata.name] != nil
+            }
+            return false
+        }
+        
         repeat {
             let element = elements[elementIndex]
-            let result = element.matches(prefix: input, isLast: isLast)
+            let result = element.matches(prefix: remainder, isLast: isLast)
             
             switch result {
             case .noMatch:
-                if let variable = currentlyActiveVariable, !input.isEmpty {
-                    currentlyActiveVariable = (variable.name, variable.value + String(input.removeFirst()), variable.interpreted, variable.acceptsNilValue, variable.map)
-                } else {
+                if !tryToAppendCurrentVariable() {
                     return .noMatch
                 }
             case .possibleMatch:
                 return .possibleMatch
             case .anyMatch(let shortest):
-                if !input.isEmpty, currentlyActiveVariable == nil, let variable = element as? VariableProtocol {
-                    currentlyActiveVariable = (variable.name, String(input.removeFirst()), variable.interpreted, variable.acceptsNilValue, variable.performMap)
-                }
-                if !shortest {
-                    if isLast, let variable = currentlyActiveVariable {
-                        if !input.isEmpty {
-                            currentlyActiveVariable = (variable.name, variable.value + String(input.removeFirst()), variable.interpreted, variable.acceptsNilValue, variable.map)
-                        } else {
-                            variables[variable.name] = finaliseVariable(variable, interpreter: interpreter)
-                            if !variable.acceptsNilValue && variables[variable.name] == nil {
+                initialiseVariable(element)
+                if shortest {
+                    elementIndex += 1
+                } else {
+                    if isLast {
+                        _ = tryToAppendCurrentVariable()
+                        if remainder.isEmpty {
+                            if !registerAndValidateVariable() {
                                 return .noMatch
                             }
                             elementIndex += 1
@@ -56,47 +78,42 @@ public class Matcher<T, E: VariableEvaluator> {
                     } else {
                         return .possibleMatch
                     }
-                } else {
-                    elementIndex += 1
                 }
             case .exactMatch(let length, _, let embeddedVariables):
-                if isEmbedded(element: element, in: String(string[start...]), at: prefix.count - input.count) {
-                    if let variable = currentlyActiveVariable, !input.isEmpty {
-                        currentlyActiveVariable = (variable.name, variable.value + String(input.removeFirst()), variable.interpreted, variable.acceptsNilValue, variable.map)
-                    } else {
+                if isEmbedded(element: element, in: String(string[start...]), at: trimmed.count - remainder.count) {
+                    if !tryToAppendCurrentVariable() {
                         elementIndex += 1
                     }
                 } else {
                     variables.merge(embeddedVariables) { (key, value) in key }
-                    if let variable = currentlyActiveVariable {
-                        variables[variable.name] = finaliseVariable(variable, interpreter: interpreter)
-                        if !variable.acceptsNilValue && variables[variable.name] == nil {
+                    if currentlyActiveVariable != nil {
+                        if !registerAndValidateVariable() {
                             return .noMatch
                         }
                         currentlyActiveVariable = nil
                     }
-                    input.removeFirst(length)
-                    input = input.trim()
+                    remainder.removeFirst(length)
+                    remainder = remainder.trim()
                     elementIndex += 1
                 }
             }
         } while elementIndex < elements.count
         
         if let renderedOutput = matcher(variables, interpreter) {
-            return .exactMatch(length: prefix.count - input.count, output: renderedOutput, variables: variables)
+            return .exactMatch(length: length - remainder.count, output: renderedOutput, variables: variables)
         } else {
             return .noMatch
         }
     }
     
-    func finaliseVariable(_ variable: (name: String, value: String, interpreted: Bool, acceptsNilValue: Bool, map: (Any, Any) -> Any?), interpreter: E) -> Any? {
+    func finaliseVariable(_ variable: (metadata: VariableProtocol, value: String), interpreter: E) -> Any? {
         let value = variable.value.trim()
-        if variable.interpreted {
+        if variable.metadata.interpreted {
             let variableInterpreter = interpreter.interpreterForEvaluatingVariables
             let output = variableInterpreter.evaluate(value)
-            return variable.map(output, variableInterpreter)
+            return variable.metadata.performMap(input: output, interpreter: variableInterpreter)
         }
-        return variable.map(value, interpreter)
+        return variable.metadata.performMap(input: value, interpreter: interpreter)
     }
     
     func isEmbedded(element: MatchElement, in string: String, at currentPosition: Int) -> Bool {
